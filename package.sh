03edc78d7457ff14e2cf54020041cd71a746e689
Tim Behrsin <tim@behrsin.com>
2012-12-08 20:46:04 +0000

    moved tgz artefact

--- package.sh
+++ package.sh
@@ -8,5 +8,5 @@ mkdir -p $BUILD_DIR/
 cp -R lib README.md LICENSE VERSION composer.json runtests.sh tests $BUILD_DIR/
 find $BUILD_DIR -name '.*' -exec rm -Rf {} \;
 cd build/
-tar czf ../../$FINAL_NAME.tar.gz $FINAL_NAME/
+tar czf ../$FINAL_NAME.tar.gz $FINAL_NAME/
 rm -Rf build/
