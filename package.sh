ef11a0fa958d3f42a9b5726cf3fc80fb524163be
Tim Behrsin <tim@behrsin.com>
2012-12-08 20:41:03 +0000

    fixes for ci

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ FINAL_NAME=app55-php-$VERSION
 BUILD_DIR=build/$FINAL_NAME
 
 mkdir -p $BUILD_DIR/
-cp -R lib README LICENSE VERSION composer.json runtests.sh tests $BUILD_DIR/
+cp -R lib README.md LICENSE VERSION composer.json runtests.sh tests $BUILD_DIR/
 find $BUILD_DIR -name '.*' -exec rm -Rf {} \;
 cd build/
 tar czf ../../$FINAL_NAME.tar.gz $FINAL_NAME/
